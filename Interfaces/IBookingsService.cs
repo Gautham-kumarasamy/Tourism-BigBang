435dba5b2a534e8b0e4f5e327d283d7153029e24
Gautham-kumarasamy <127299009+Gautham-kumarasamy@users.noreply.github.com>
2023-08-04 23:49:33 +0530

    Add files via upload

--- Interfaces/IBookingsService.cs
+++ Interfaces/IBookingsService.cs
@@ -1,7 +1,12 @@
-﻿namespace MakeYourTrip.Interfaces
+﻿using MakeYourTrip.Models;
+
+namespace MakeYourTrip.Interfaces
 {
     public interface IBookingsService
     {
-        
+
+        Task<Booking> Add_Bookings(Booking booking);
+        Task<List<Booking>?> View_All_bookings();
+
     }
 }
--- Interfaces/IRoomBookingsService.cs
+++ Interfaces/IRoomBookingsService.cs
@@ -1,6 +1,13 @@
-﻿namespace MakeYourTrip.Interfaces
+﻿using MakeYourTrip.Models;
+using MakeYourTrip.Services;
+
+namespace MakeYourTrip.Interfaces
 {
     public interface IRoomBookingsService
     {
+
+
+        Task<RoomBooking> Add_RoomBookingsService(RoomBooking roomBookingsService);
+        Task<List<RoomBooking>?> View_All_RoomBookingsService();
     }
 }
